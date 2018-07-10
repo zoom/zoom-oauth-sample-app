ad466b38d9ec2b4d5791840bf68bcdb38f482898
Michael Purnell <michaelpurnell@Michael-Purnell-57.local>
2018-07-10 10:06:28 -0700

    added config.js file

--- config.js
+++ config.js
@@ -3,12 +3,14 @@ const env = process.env.NODE_ENV || 'development'
 //insert your API Key & Secret for each environment, keep this file local and never push it to repo.
 const config = {
 	development :{
-		APIKey : '',
-		APISecret : ''
+		clientID : '',
+		clientSecret : '',
+		redirectUrl : ''
 	},
 	production:{	
-		APIKey : '',
-		APISecret : ''
+		clientID : '',
+		clientSecret : '',
+		redirectUrl: ''
 	}
 };
 
