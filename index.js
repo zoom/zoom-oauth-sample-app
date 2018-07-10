97a9df4328db87dcec4e1f55b0b3704a15be4ca2
Michael Purnell <michaelpurnell@Michael-Purnell-57.local>
2018-07-10 10:06:12 -0700

    index.js file for sample oaith call

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const app = express()
 const config = require('./config');
-const rp = require('request-promise');
+const request = require('request');
 
-const payload = {
-    iss: config.APIKey,
-    exp: ((new Date()).getTime() + 5000)
-};
+app.get('/', (req, res) => {
 
-//Automatically creates header, and returns JWT
-const token = jwt.sign(payload, config.APISecret);
+    //this will check if the code parameter is in the url, if not the most likely case is that this is the user's inital visit to oauth and we need to redirect them (Step 1)
+    //if there is a code, it most likely means they were redirected here from zoom oauth screen
+    if (req.query.code) {
 
+        let url = 'https://zoom.us/oauth/token?grant_type=authorization_code&code=' + req.query.code + '&redirect_uri=' + config.redirectUrl;
 
-var options = {
-    uri: 'https://api.zoom.us/v2/users', //Zoom API
-    qs: {
-        status: 'active' // -> uri + '?status=active'
-    },
-    auth: {
-    		'bearer': token
-  	},
-    headers: {
-        'User-Agent': 'Zoom-api-Jwt-Request',
-        'content-type': 'application/json'
-    },
-    json: true // Automatically parses the JSON string in the response
-};
+        //STEP 3
+        //we need to exchange the code for a oauth token
+        request.post(url, function (error, response, body) {
 
-//using request call with promise
-rp(options)
-    .then(function (response) {
-        console.log('User has', response);
-    })
-    .catch(function (err) {
-        // API call failed...
-        console.log('API call failed, reason ', err);
-    });
+            //the response should be a JSON payload
+            body = JSON.parse(body);
+            
+            //get refresh token
+            let refresh_token = body.refresh_token
+
+            if (body.access_token) {
+
+                //STEP 4
+                //we can now use the access token to make API calls
+                request.get('https://api.zoom.us/v2/users/me', function (error, response, body) {
+                    if(error){
+                        console.log('Error in API ', error)
+                    }else{
+                        body = JSON.parse(body);
+                        console.log('API call ', body);
+                        //do something with the data
+                        //this is most likely where you want to connect the zoom user to the calendly user, there will be a zoom user id
+                        //add where you'll want to store the access token for future requests
+                    }
+                }).auth(null, null, true, body.access_token);
+            
+            } else {
+                //handle error, something went wrong
+            }
+
+        }).auth(config.clientID, config.clientSecret);
+
+        return;
+    }
+
+    //STEP 2
+    //no code provided, so redirect the user to get the code
+    res.redirect('https://zoom.us/oauth/authorize?response_type=code&client_id=' + config.clientID + '&redirect_uri=' + config.redirectUrl);
+});
+
+app.listen(3000, () => console.log('Zoom api oauth sample app listening on port 3000!'))
+ 
