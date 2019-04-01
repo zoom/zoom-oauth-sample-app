1e10fbb82aa6e2d160cca32e780410135044e603
ShrijanaCodes <29689264+ShrijanaCodes@users.noreply.github.com>
2019-04-01 11:46:11 -0700

    Removed credentials
    I pushed the credentials of my app by mistake. Removed it now.

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret for each environment, keep this file local and never push it to repo.
 const config = {
 	development :{
-		clientID : 'owl34V64TVrX2gYAKBUvQ',
-		clientSecret : '4YZ2XLTfQMmt6J1htGB33T5PMEy2XwBT',
-		redirectUrl : 'https://080b95ee.ngrok.io'
+		clientID : '',
+		clientSecret : '',
+		redirectUrl : ''
 	},
 	production:{	
-		clientID : '7W1DsbORUSYvRB1M9C0Q',
-		clientSecret : 'YkPqAqaVRWZwrPbCgIvUEyZQbe9UO6jp',
-		redirectUrl: 'https://080b95ee.ngrok.io'
+		clientID : '',
+		clientSecret : '',
+		redirectUrl: ''
 	}
 };
 
