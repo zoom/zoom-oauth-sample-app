bd9a5b0f64cba994c74bcf0cbe46b3486efbd5ed
ShrijanaCodes <29689264+ShrijanaCodes@users.noreply.github.com>
2019-04-01 11:46:11 -0700

    Removed credentials
    I pushed the credentials of my app by mistake. Removed it now.

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret for each environment, keep this file local and never push it to repo.
 const config = {
 	development :{
-		clientID : '***REMOVED***',
-		clientSecret : '***REMOVED***',
-		redirectUrl : '***REMOVED***'
+		clientID : '',
+		clientSecret : '',
+		redirectUrl : ''
 	},
 	production:{	
-		clientID : '***REMOVED***',
-		clientSecret : '***REMOVED***',
-		redirectUrl: '***REMOVED***'
+		clientID : '',
+		clientSecret : '',
+		redirectUrl: ''
 	}
 };
 
