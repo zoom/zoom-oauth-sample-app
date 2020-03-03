8aed6594ba3fb810267e29b1603ea32ef5fa2385
michaeldharrington <michael.harrington@zoom.us>
2020-03-03 14:32:53 -0700

    v2 init

--- index.js
+++ index.js
@@ -1,71 +1,93 @@
+// Bring in environment secrets through dotenv
+require('dotenv/config')
+
+// Use the request module to make HTTP requests from Node
+const request = require('request')
+
+// Run the express app
 const express = require('express')
 const app = express()
-const config = require('./config');
-const request = require('request');
-var resp;
+
 
 app.get('/', (req, res) => {
 
-    //this will check if the code parameter is in the url, if not the most likely case is that this is the user's inital visit to oauth and we need to redirect them (Step 1)
-    //if there is a code, it most likely means they were redirected here from zoom oauth screen
+    // Step 1: 
+    // Check if the code parameter is in the url 
+    // if an authorization code is available, the user has most likely been redirected from Zoom OAuth
+    // if not, the user needs to be redirected to Zoom OAuth to authorize
+
     if (req.query.code) {
 
-        let url = 'https://zoom.us/oauth/token?grant_type=authorization_code&code=' + req.query.code + '&redirect_uri=' + config.redirectUrl;
+        // Step 3: 
+        // Request an access token using the auth code
 
-        //STEP 3
-        //we need to exchange the code for a oauth token
-        request.post(url, function (error, response, body) {
+        let url = 'https://zoom.us/oauth/token?grant_type=authorization_code&code=' + req.query.code + '&redirect_uri=' + process.env.redirectURL;
 
-            //the response should be a JSON payload
+        request.post(url, (error, response, body) => {
+
+            // Parse response to JSON
             body = JSON.parse(body);
-            //html for displaying response in the browser
-            var title1 ='<center><h3>Your access token: </h3></center>' 
-            var title2 ='<center><h3>Your refresh token: </h3></center>' 
-            var result1 = title1 + '<code><pre style="background-color:#aef8f9;">' + body.access_token + '</pre></code>';
-            var result2 = title2 + '<code><pre style="background-color:#aef8f9;">' + body.refresh_token + '</pre></code>';
 
-            var title ='<center><h3>User\'s information:</h3></center>' 
-        //Prettify the JSON format using pre tag and JSON.stringify
-            
-            //print access_token in the browser
-            //res.send(body.access_token); 
-            console.log(body.access_token);
-            
-            //get refresh token
-            let refresh_token = body.refresh_token
+            // Logs your access and refresh tokens in the browser
+            console.log(`access_token: ${body.access_token}`);
+            console.log(`refresh_token: ${body.refresh_token}`);
 
             if (body.access_token) {
 
-                //STEP 4
-                //we can now use the access token to make API calls
-                request.get('https://api.zoom.us/v2/users/me', function (error, response, body) {
-                    if(error){
-                        console.log('Error in API ', error)
-                    }else{
-                        body = JSON.parse(body);
-                        //display response in console
-                        console.log('API call ', body);
-                        //display response in browser
-                        var result = title + '<code><pre style="background-color:#aef8f9;">'+JSON.stringify(body, null, 2)+ '</pre></code>'
-                        res.send(result1 + '<br>' + result2 + '<br>' +  result);
- 
-                    }
+                // Step 4:
+                // We can now use the access token to authenticate API calls
 
+                // Send a request to get your user information using the /me context
+                // The `/me` context restricts an API call to the user the token belongs to
+                // This helps make calls to user-specific endpoints instead of storing the userID
+
+                request.get('https://api.zoom.us/v2/users/me', (error, response, body) => {
+                    if (error) {
+                        console.log('API Response Error: ', error)
+                    } else {
+                        body = JSON.parse(body);
+                        // Display response in console
+                        console.log('API call ', body);
+                        // Display response in browser
+                        var JSONResponse = '<pre><code>' + JSON.stringify(body, null, 2) + '</code></pre>'
+                        res.send(`
+                            <style>
+                                @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');@import url('https://necolas.github.io/normalize.css/8.0.1/normalize.css');html {color: #232333;font-family: 'Open Sans', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}h2 {font-weight: 700;font-size: 24px;}h4 {font-weight: 600;font-size: 14px;}.container {margin: 24px auto;padding: 16px;max-width: 720px;}.info {display: flex;align-items: center;}.info>div>span, .info>div>p {font-weight: 400;font-size: 13px;color: #747487;line-height: 16px;}.info>div>span::before {content: "👋";}.info>div>h2 {padding: 8px 0 6px;margin: 0;}.info>div>p {padding: 0;margin: 0;}.info>img {background: #747487;height: 96px;width: 96px;border-radius: 31.68px;overflow: hidden;margin: 0 20px 0 0;}.response {margin: 32px 0;display: flex;flex-wrap: wrap;align-items: center;justify-content: space-between;}.response>a {text-decoration: none;color: #2D8CFF;font-size: 14px;}.response>pre {overflow-x: scroll;background: #f6f7f9;padding: 1.2em 1.4em;border-radius: 10.56px;width: 100%;box-sizing: border-box;}
+                            </style>
+                            <div class="container">
+                                <div class="info">
+                                    <img src="${body.pic_url}" alt="User photo" />
+                                    <div>
+                                        <span>Hello World!</span>
+                                        <h2>${body.first_name} ${body.last_name}</h2>
+                                        <p>${body.role_name}, ${body.company}</p>
+                                    </div>
+                                </div>
+                                <div class="response">
+                                    <h4>JSON Response:</h4>
+                                    <a href="https://marketplace.zoom.us/docs/api-reference/zoom-api/users/user" target="_blank">
+                                        API Reference
+                                    </a>
+                                    ${JSONResponse}
+                                </div>
+                            </div>
+                        `);
+                    }
                 }).auth(null, null, true, body.access_token);
-            
+
             } else {
-                //handle error, something went wrong
+                // Handle errors, something's gone wrong!
             }
 
-        }).auth(config.clientID, config.clientSecret);
+        }).auth(process.env.clientID, process.env.clientSecret);
 
         return;
+
     }
 
-    //STEP 2
-    //no code provided, so redirect the user to get the code
-    res.redirect('https://zoom.us/oauth/authorize?response_type=code&client_id=' + config.clientID + '&redirect_uri=' + config.redirectUrl);
-});
+    // Step 2: 
+    // If no authorization code is available, redirect to Zoom OAuth to authorize
+    res.redirect('https://zoom.us/oauth/authorize?response_type=code&client_id=' + process.env.clientID + '&redirect_uri=' + process.env.redirectURL)
+})
 
-app.listen(3000, () => console.log('Zoom api oauth sample app listening on port 3000!'))
- 
+app.listen(4000, () => console.log(`Zoom Hello World app listening at PORT: 4000`))
